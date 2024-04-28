9bc85019baa5c93566fc81a3c6ef27e16cf7b27e
annadelehanty <agmdelehanty@gmail.com>
2024-04-28 13:48:31 +0100

    made the app!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  let [city, setCity] = useState("");
+  let [temperature, setTemperature] = useState(null);
+  let [description, setDescription] = useState("");
+  let [humidity, setHumidity] = useState(null);
+  let [windSpeed, setWindSpeed] = useState(null);
+  let [iconUrl, setIconUrl] = useState("");
+  let [iconAlt, setIconAlt] = useState("");
+  let apiKey = "4288f539432426do920341baabbb0tad";
+  function createDataset(response) {
+    setTemperature(response.data.temperature.current);
+    setDescription(response.data.condition.description);
+    setHumidity(response.data.temperature.humidity);
+    setWindSpeed(response.data.wind.speed);
+    setIconUrl(response.data.condition.icon_url);
+    setIconAlt(response.data.condition.icon);
+  }
+  function getData(event) {
+    let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(url).then(createDataset);
+    event.preventDefault();
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  if (temperature) {
+    return (
+      <div>
+        <form onSubmit={getData}>
+          <input
+            type="search"
+            placeholder="enter a city..."
+            onChange={updateCity}
+          />
+          <input type="submit" />
+        </form>
+        <ul>
+          <li>Temperature: {Math.round(temperature)}°C</li>
+          <li>Description: {description}</li>
+          <li>Humidity: {humidity}%</li>
+          <li>Wind: {windSpeed}km/h</li>
+          <li>
+            <img src={iconUrl} alt={iconAlt} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <form onSubmit={getData}>
+        <input
+          type="search"
+          placeholder="enter a city..."
+          onChange={updateCity}
+        />
+        <input type="submit" />
+      </form>
+    );
+  }
 }
-
-export default App;
