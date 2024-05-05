0bcfdd29ebecae01dc280e979dc2abbb38c427cc
annadelehanty <agmdelehanty@gmail.com>
2024-05-05 15:52:14 +0100

    removed previous work

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
-  useEffect(() => {
-    setLoaded(false);
-  }, [props]);
   function handleResponse(response) {
     setForecast(response.data);
     setLoaded(true);
