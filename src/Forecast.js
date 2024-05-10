d43461601001141a01cf172f83431cafa6b5fcc4
annadelehanty <agmdelehanty@gmail.com>
2024-05-10 09:50:48 +0100

    made the function return null if no forecast is loaded

--- src/Forecast.js
+++ src/Forecast.js
@@ -5,32 +5,25 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
   useEffect (() => {
-    setLoaded(false);
+    const apiKey = "4288f539432426do920341baabbb0tad";
+    let url = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}`;
+
+    axios.get(url).then((response) => {
+      setForecast(response.data.daily);
+      setLoaded(true);
+    });
   }, [props.city]);
-  function handleResponse(response) {
-    setForecast(response.data.daily);
-    setLoaded(true);
-  }
+
   if (loaded) {
     return (
       <div className="row col-xs-12 col-sm-6">
-        <WeatherForecastDay data={forecast[1]} />
-        <hr />
-        <WeatherForecastDay data={forecast[2]} />
-        <hr />
-        <WeatherForecastDay data={forecast[3]} />
-        <hr />
-        <WeatherForecastDay data={forecast[4]} />
-        <hr />
-        <WeatherForecastDay data={forecast[5]} />
+        {forecast && <WeatherForecastDay data={forecast[1]} />}
         <hr />
       </div>
     );
   } else {
-    const apiKey = "4288f539432426do920341baabbb0tad";
-    let url = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}`;
-    axios.get(url).then(handleResponse);
-    return <div className="row col-xs-12 col-sm-6">loading forecast...</div>;
+    return null;
   }
 }
